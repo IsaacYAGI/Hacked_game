ce48a6671a358ed6439d44bca474b86450a0dda0
Isaac Yriarte <isaacyriarte@gmail.com>
2016-02-28 00:37:39 -0430

    Agregado respuesta de nivel bring some order

---
+++ Cheatcode/bring_some_order.h
@@ -0,0 +1,17 @@
+var_a = "";
+var_b = false;
+
+while !var_b {
+	var_b = true;
+	var_i = 0;
+	while var_i < input.lenght - 1{		
+		if input[var_i] > input[var_i + 1]{
+			var_a = input[var_i + 1];
+			input[var_i + 1] = input[var_i];
+			input[var_i] = var_a;
+			var_b = false;
+		}
+		var_i++;
+	}
+}
+return input;
