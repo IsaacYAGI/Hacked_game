485157a62b031519fde6fb9b55f25cfdeca0ffeb
Isaac Yriarte <isaacyriarte@gmail.com>
2016-03-02 23:04:59 -0430

    Se agrega la solucion a los dos primeros problemas del nivel Nuclear Plant

---
+++ Nuclear_Plant/add_one.h
@@ -0,0 +1,5 @@
+while var_i < input.lenght{
+	input[var_i] = input[var_i] + 1;
+	var_i++;
+}
+return input;
---
+++ Nuclear_Plant/positivity.h
@@ -0,0 +1,10 @@
+function f1: var_a{
+	return var_a == 0 || var_a > 0;
+}
+
+var_b = [];
+
+while var_i < input.lenght{
+	var_b.push(f1(input[var_i++]));
+}
+return var_b;
